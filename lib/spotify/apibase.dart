b0a2af25858abc2cf2cd5d6e347db3c8d9cae753
EmoneyCode <trame718@gmail.com>
2025-10-26 16:50:24 -0500

    added api base state

---
+++ lib/spotify/apibase.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class Apibase extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+  
+}
+
+class _apiBaseState extends State{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
